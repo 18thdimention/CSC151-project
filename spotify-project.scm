a929beb58192fee885f8e42fb749dca622de85f3
Nick <ndavraz5@gmail.com>
2025-12-07 22:20:36 -0600

    Update spotify-project.scm
    reorganized testcases, added documentation/headers (moved functions around), added graphs for average track popularity, customized graphs

--- spotify-project.scm
+++ spotify-project.scm
@@ -11,6 +11,36 @@
 (import test)
 (import lab)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define list-of-days
+  (list "Sunday" "Monday" "Tuesday" "Wednesday" "Thursday"
+        "Friday" "Saturday"))
+
+(define list-of-months
+  (list "January" "February" "March" "April" "May" "June" "July"
+        "August" "September" "October" "November" "December"))
+
+;;; days-in-months
+;;; Defines the number of days in each month.
+(define days-in-months
+  (list (pair 1 31)
+        (pair 2 28) 
+        (pair 3 31)
+        (pair 4 30)
+        (pair 5 31)
+        (pair 6 30)
+        (pair 7 31)
+        (pair 8 31)
+        (pair 9 30)
+        (pair 10 31)
+        (pair 11 30)
+        (pair 12 31)))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; multi-purpose functions ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,10 +63,6 @@
             (cadr time-list)
             (caddr time-list)))))
 
-(test-case "string->time: 2025-10-23"
-  equal? (time 2025 10 23)
-  (lambda () (string->time "2025-10-23")))
-
 ;;; (get-release-date list) -> string?
 ;;;   list: list?, row of csv file
 ;;; Given a parsed csv file, Returns the release date
@@ -58,26 +84,27 @@
 (define get-artist-popularity
   (section list-ref _ 6))
 
+;;; (chart-tally x-labels dataset-options dataset-name tally) -> histogram?
+;;;   dataset-options: list of key-value pairs
+;;;   x-labels: list? of string?, (equal? (length x-labels) (length tally))
+;;;   dataset-name: string?
+;;;   tally : assoc-list?
+;;; Plots a histogram with the y axis consisting of the cdr of 
+;;; every tally pair and the x axis consisting of the list of strings.
+(define chart-tally
+  (lambda (x-labels dataset-options dataset-name tally)
+    (plot-category
+      x-labels
+      (with-dataset-options
+        dataset-options
+        (dataset-bar
+          dataset-name
+          (map cdr tally))))))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; day-of-week implementation ;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; days-in-months
-;;; Defines the number of days in each month.
-(define days-in-months
-  (list (pair 1 31)
-        (pair 2 28) 
-        (pair 3 31)
-        (pair 4 30)
-        (pair 5 31)
-        (pair 6 30)
-        (pair 7 31)
-        (pair 8 31)
-        (pair 9 30)
-        (pair 10 31)
-        (pair 11 30)
-        (pair 12 31)))
-
 
 ;;; (leap-year? year) -> boolean?
 ;;;   year : integer?
@@ -88,23 +115,6 @@
         (and (not (= 0 (remainder year 100)))
              (= 0 (remainder year 4))))))
 
-(test-case "leap-year?: divisible by 100"
-  equal? #f
-  (lambda () (leap-year? 1900)))
-
-(test-case "leap-year?: divisible by 400"
-  equal? #t
-  (lambda () (leap-year? 2000)))
-
-(test-case "leap-year?: 2020"
-  equal? #t
-  (lambda () (leap-year? 2020)))
-
-(test-case "leap-year?: 2024"
-  equal? #t
-  (lambda () (leap-year? 2024)))
-
-
 ;;; (days-in-year year) -> integer?
 ;;;   year : integer?
 ;;; Checks if a day is a leap year and
@@ -114,15 +124,6 @@
     (if (leap-year? year)
         366
         365)))
-
-(test-case "days-in-year: 2024"
-  equal? 366
-  (lambda () (days-in-year 2024)))
-
-(test-case "days-in-year: 2023"
-  equal? 365
-  (lambda () (days-in-year 2023)))
-
 
 ;;; (months->days-helper month months-list) -> integer?
 ;;;   month : integer?
@@ -147,23 +148,6 @@
         (months->days-helper month (assoc-set 2 29 days-in-months))
         (months->days-helper month days-in-months))))
 
-(test-case "months->days: january nonleap"
-  equal? 31
-  (lambda () (months->days 1 #f)))
-
-(test-case "months->days: january leap"
-  equal? 31
-  (lambda () (months->days 1 #t)))
-
-(test-case "months->days: march nonleap"
-  equal? 90
-  (lambda () (months->days 3 #f)))
-
-(test-case "months->days: march leap"
-  equal? 91
-  (lambda () (months->days 3 #t)))
-
-
 ;;; (years->days year) -> integer?
 ;;;   year : integer?
 ;;;  Returns the number of days in the years since 1950.
@@ -172,27 +156,6 @@
     (match year
       [1950 0]
       [_ (+ (days-in-year (- year 1)) (years->days (- year 1)))])))
-
-(test-case "years->days: 1950"
-  equal? 0
-  (lambda () (years->days 1950)))
-
-(test-case "years->days: 1971"
-  equal? 7670
-  (lambda () (years->days 1971)))
-
-(test-case "years->days: 2025"
-  equal? 27394
-  (lambda () (years->days 2025)))
-
-(test-case "years->days: 2001" 
-  equal? 18628
-  (lambda () (years->days 2001)))
-
-(test-case "years->days: 2000" ;;;FAILED! expected 10,957 recieved 10,958;;;
-  equal? 18262
-  (lambda () (years->days 2000)))
-
 
 ;;; (time->timestamp time) -> integer?
 ;;;   time : time?
@@ -205,28 +168,6 @@
             (years->days (time-year time)))
          1)))
 
-
-(test-case "time->timestamp: 2025-11-24"
-  equal? 27721
-  (lambda () (time->timestamp (time 2025 11 24))))
-
-(test-case "time->timestamp: 2024-5-10"
-  equal? 27158
-  (lambda () (time->timestamp (time 2024 5 10))))
-
-(test-case "time->timestamp: 2000-7-21"
-  equal? 18464
-  (lambda () (time->timestamp (time 2000 7 21))))
-
-(test-case "time->timestamp: 1970-12-21"
-  equal? 7659
-  (lambda () (time->timestamp (time 1970 12 21))))
-
-(test-case "time->timestamp: 1999-1-1" 
-  equal? 17897
-  (lambda () (time->timestamp (time 1999 1 1))))
-
-
 ;;; (day-of-week time) -> integer?
 ;;;   time : time?
 ;;; Converts a time into a day of the week,
@@ -237,26 +178,7 @@
       (if (< day-offset 0)
           (+ day-offset 7)
           day-offset))))
-        
-(test-case "day-of-week: 2025-11-24 (monday)"
-  equal? 1
-  (lambda () (day-of-week (time 2025 11 24))))
 
-(test-case "day-of-week: 2024-5-10 (friday)"
-  equal? 5
-  (lambda () (day-of-week (time 2024 5 10))))
-
-(test-case "day-of-week: 1952-5-12 (Thursday)"
-  equal? 4
-  (lambda () (day-of-week (time 1952 6 12))))
-
-(test-case "day-of-week: 1956-07-28 (Saturday)"
-  equal? 6
-  (lambda () (day-of-week (time 1956 7 28))))
-
-(test-case "day-of-week: 1964-12-16 (Wednesday)"
-  equal? 3
-  (lambda () (day-of-week (time 1964 12 16))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; visualizing number of tracks by date ;;;;;;;;;;;
@@ -273,41 +195,20 @@
       (map (lambda (key) (pair key (assoc-ref key tally)))
         sorted-keys))))
 
-(test-case "sort-tally-<: numeric tally"
-  equal? (list (pair 0 234) (pair 1 34) (pair 12 23) (pair 234 2))
-  (lambda () (sort-tally-< 
-               (list (pair 0 234) (pair 12 23) (pair 234 2) (pair 1 34)))))
+;;; (tally-by-time proc data) -> list?
+;;;   proc : procedure?, operates on a time and 
+;;;          returns an integer representing a specific element.
+;;;   data : string? from a file, csv format.
+(define tally-by-time
+  (lambda (proc data)
+    (sort-tally-<
+      (tally-all
+        (map (o proc string->time get-release-date)
+          (parse-csv data))))))
 
-;;; (chart-days tally) -> histogram?
-;;;   tally : assoc-list?
-;;; Given an assoc-list with keys in numeric
-;;; order from 0 to 6 representing days of the week,
-;;; returns a histogram plotting those values.
-(define chart-days
-  (lambda (tally)
-    (plot-category
-      (list "Sunday" "Monday" "Tuesday" "Wednesday"
-            "Thursday" "Friday" "Saturday")
-      (dataset-bar
-        "Tallies"
-        (map cdr tally)))))
-
-;;; (chart-months tally) -> histogram?
-;;;   tally : assoc-list?
-;;; Given an assoc-list with keys in numeric
-;;; order from 1 to 12 representing months,
-;;; returns a histogram plotting those values.
-(define chart-months
-  (lambda (tally)
-    (plot-category
-      (list "January" "February" "March" "April" "May" "June" 
-            "July" "August" "September" "October" "November" "December")
-        (dataset-bar
-          "Tallies"
-          (map cdr tally)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; implementing average popularity by date ;;;;;;;;
+;;;; implementing average popularity by time ;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; (time-popularity-pair proc csv-line) -> pair? (of integer?)
@@ -364,29 +265,6 @@
   (lambda (pairs n)
     (total-and-number-helper pairs n 0 0)))
 
-
-(test-case "total-and-number: empty list"
-  equal? (list (pair 0 (list 0 0)))
-  (lambda () (total-and-number null 0)))
-
-(test-case "total-and-number: one pair"
-  equal? (list (pair 0 (list 12 1)))
-  (lambda () (total-and-number (list (pair 0 12)) 0)))
-
-(test-case "total-and-number: multiple pairs pair"
-  equal? (list (pair 0 (list 55 3)))
-  (lambda () (total-and-number (list (pair 0 12) (pair 0 20) (pair 0 23)) 0)))
-
-
-(test-case "total-and-number: n increments"
-  equal? (list (pair 0 (list 55 3)) (pair 1 (list 43 2)))
-  (lambda () (total-and-number (list (pair 0 12) (pair 0 20) (pair 0 23) (pair 1 23)
-                                     (pair 1 20)) 0)))
-
-(test-case "total-and-number: start with 1"
-  equal? (list (pair 1 (list 43 2)))
-  (lambda () (total-and-number (list (pair 1 23)
-                                     (pair 1 20)) 1)))
 ;;; (average pair1) -> pair?
 ;;;   pair1 : pair?
 ;;; Given a pair containing a head element and
@@ -400,21 +278,10 @@
                        (cadr tail))])
       (pair head average))))
 
-;;; (tally-by-time proc data) -> list?
-;;;   proc : procedure?, operates on a time and 
-;;;          returns an integer representing a specific element.
-;;;   data : string? from a file, csv format.
-(define tally-by-time
-  (lambda (proc data)
-    (sort-tally-<
-      (tally-all
-        (map (o proc string->time get-release-date)
-          (parse-csv data))))))
-
 ;;; (average-popularity-by-time proc data) -> list?
 ;;;   proc: procedure?, takes one row as input and outputs a pair
-;;;         of an integer representing a time element and the track
-;;;         popularity.
+;;;         of an integer representing a time element and and integer
+;;;         representing the track popularity.
 ;;;   data: string? from a file, csv format.
 (define average-popularity-by-time
   (lambda (proc data)
@@ -428,6 +295,7 @@
       (map average
         (total-and-number sorted-data n)))))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; HIGH LEVEL / USER-LEVEL ALGORITHMNS ;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -435,33 +303,65 @@
 (description "Day of the Week VS Released Tracks")
 
 ;;; DAYS BAR GRAPH ;;;
+
+(define days-dataset-options
+  (list (pair "background-color" "darkgreen")))
+
 (with-file-chooser
   (lambda (data)
-    (chart-days 
-      (tally-by-time day-of-week data))))
+    (with-plot-options
+      (list (pair "title" "Amount of released tracks vs. day of week")
+            (pair "x-label" "Days")
+            (pair "y-label" "Number of tracks"))
+      (chart-tally list-of-days days-dataset-options "Amount released on given day"
+        (tally-by-time day-of-week data)))))
 
 (description "Month of the Year VS Released Tracks")
 
 ;;; MONTHS BAR GRAPH ;;;
+
+(define months-dataset-options
+  (list (pair "background-color" "red")))
+
 (with-file-chooser
   (lambda (data)
-    (chart-months
-      (tally-by-time time-month data))))
+    (with-plot-options
+      (list (pair "title" "Amount of released tracks vs. month")
+            (pair "x-label" "Months")
+            (pair "y-label" "Number of tracks"))
+      (chart-tally list-of-months months-dataset-options "Amount released in given month"
+        (tally-by-time time-month data)))))
 
 (description "Average track popularity by day")
 
 ;;; AVERAGE TRACK POPULARITY BY DAY ;;;
+(define day-pop-dataset-options
+  (list (pair "background-color" "orange")))
+
 (with-file-chooser
   (lambda (data)
-    (average-popularity-by-time
-      (section time-popularity-pair day-of-week _) data)))
+    (with-plot-options
+      (list (pair "title" "Average track popularity vs. day of week")
+            (pair "x-label" "Days")
+            (pair "y-label" "Average track popularity"))
+      (chart-tally list-of-days day-pop-dataset-options "Average track popularity on given day"
+        (average-popularity-by-time
+          (section time-popularity-pair day-of-week _) data)))))
 
 (description "Average track popularity by month")
 ;;; AVERAGE TRACK POPULARITY BY MONTH
+(define month-pop-dataset-options
+  (list (pair "background-color" "pink")))
+  
 (with-file-chooser
   (lambda (data)
-    (average-popularity-by-time
-      (section time-popularity-pair time-month _) data)))
+    (with-plot-options
+      (list (pair "title" "Average track popularity vs. month")
+            (pair "x-label" "Months")
+            (pair "y-label" "Average track popularity"))
+      (chart-tally list-of-months month-pop-dataset-options "Average track popularity in given month"
+        (average-popularity-by-time
+          (section time-popularity-pair time-month _) data)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;; mayu's stuff ;;;;
@@ -488,6 +388,7 @@ track-popularity
         (parse-csv data)))))
 
 artist-popularity
+
 
 ;;; (track-artist data) -> list?
 ;;;   data: data?
@@ -526,6 +427,139 @@ track-artist
           (map (o string->number get-artist-popularity)
             (parse-csv data)))))))
 
-(with-file-chooser
-  (lambda (data)
-    (parse-csv data)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; test cases ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(test-case "string->time: 2025-10-23"
+  equal? (time 2025 10 23)
+  (lambda () (string->time "2025-10-23")))
+
+(test-case "leap-year?: divisible by 100"
+  equal? #f
+  (lambda () (leap-year? 1900)))
+
+(test-case "leap-year?: divisible by 400"
+  equal? #t
+  (lambda () (leap-year? 2000)))
+
+(test-case "leap-year?: 2020"
+  equal? #t
+  (lambda () (leap-year? 2020)))
+
+(test-case "leap-year?: 2024"
+  equal? #t
+  (lambda () (leap-year? 2024)))
+
+(test-case "days-in-year: 2024"
+  equal? 366
+  (lambda () (days-in-year 2024)))
+
+(test-case "days-in-year: 2023"
+  equal? 365
+  (lambda () (days-in-year 2023)))
+
+(test-case "months->days: january nonleap"
+  equal? 31
+  (lambda () (months->days 1 #f)))
+
+(test-case "months->days: january leap"
+  equal? 31
+  (lambda () (months->days 1 #t)))
+
+(test-case "months->days: march nonleap"
+  equal? 90
+  (lambda () (months->days 3 #f)))
+
+(test-case "months->days: march leap"
+  equal? 91
+  (lambda () (months->days 3 #t)))
+
+(test-case "years->days: 1950"
+  equal? 0
+  (lambda () (years->days 1950)))
+
+(test-case "years->days: 1971"
+  equal? 7670
+  (lambda () (years->days 1971)))
+
+(test-case "years->days: 2025"
+  equal? 27394
+  (lambda () (years->days 2025)))
+
+(test-case "years->days: 2001" 
+  equal? 18628
+  (lambda () (years->days 2001)))
+
+(test-case "years->days: 2000"
+  equal? 18262
+  (lambda () (years->days 2000)))
+
+(test-case "time->timestamp: 2025-11-24"
+  equal? 27721
+  (lambda () (time->timestamp (time 2025 11 24))))
+
+(test-case "time->timestamp: 2024-5-10"
+  equal? 27158
+  (lambda () (time->timestamp (time 2024 5 10))))
+
+(test-case "time->timestamp: 2000-7-21"
+  equal? 18464
+  (lambda () (time->timestamp (time 2000 7 21))))
+
+(test-case "time->timestamp: 1970-12-21"
+  equal? 7659
+  (lambda () (time->timestamp (time 1970 12 21))))
+
+(test-case "time->timestamp: 1999-1-1" 
+  equal? 17897
+  (lambda () (time->timestamp (time 1999 1 1))))
+
+(test-case "day-of-week: 2025-11-24 (monday)"
+  equal? 1
+  (lambda () (day-of-week (time 2025 11 24))))
+
+(test-case "day-of-week: 2024-5-10 (friday)"
+  equal? 5
+  (lambda () (day-of-week (time 2024 5 10))))
+
+(test-case "day-of-week: 1952-5-12 (Thursday)"
+  equal? 4
+  (lambda () (day-of-week (time 1952 6 12))))
+
+(test-case "day-of-week: 1956-07-28 (Saturday)"
+  equal? 6
+  (lambda () (day-of-week (time 1956 7 28))))
+
+(test-case "day-of-week: 1964-12-16 (Wednesday)"
+  equal? 3
+  (lambda () (day-of-week (time 1964 12 16))))
+
+(test-case "sort-tally-<: numeric tally"
+  equal? (list (pair 0 234) (pair 1 34) (pair 12 23) (pair 234 2))
+  (lambda () (sort-tally-< 
+               (list (pair 0 234) (pair 12 23) (pair 234 2) (pair 1 34)))))
+
+(test-case "total-and-number: empty list"
+  equal? (list (pair 0 (list 0 0)))
+  (lambda () (total-and-number null 0)))
+
+(test-case "total-and-number: one pair"
+  equal? (list (pair 0 (list 12 1)))
+  (lambda () (total-and-number (list (pair 0 12)) 0)))
+
+(test-case "total-and-number: multiple pairs pair"
+  equal? (list (pair 0 (list 55 3)))
+  (lambda () (total-and-number (list (pair 0 12) (pair 0 20) (pair 0 23)) 0)))
+
+
+(test-case "total-and-number: n increments"
+  equal? (list (pair 0 (list 55 3)) (pair 1 (list 43 2)))
+  (lambda () (total-and-number (list (pair 0 12) (pair 0 20) (pair 0 23) (pair 1 23)
+                                     (pair 1 20)) 0)))
+
+(test-case "total-and-number: start with 1"
+  equal? (list (pair 1 (list 43 2)))
+  (lambda () (total-and-number (list (pair 1 23)
+                                     (pair 1 20)) 1)))
